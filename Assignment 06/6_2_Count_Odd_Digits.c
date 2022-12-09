76b14cfa62c0e6a712b21cf669a2d384cce57dad
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-09 11:44:08 +0530

    Add files via upload

---
+++ Assignment 06/6_2_Count_Odd_Digits.c
@@ -0,0 +1,60 @@
+
+/*
+2.Write a program which accept number from user and return the count of odd
+digits.
+
+Input : 2395
+Output : 3
+
+Input : 1018
+Output : 2
+
+Input : -1018
+Output : 2
+
+Input : 8462
+Output : 0
+
+*/
+
+#include<stdio.h>
+
+int CountOdd(int iNo)
+{
+    int iDigit = 0;
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        
+        if((iDigit % 2) != 0)
+        {
+            iCnt++;
+        }
+
+        iNo = iNo / 10;
+    }
+
+    return iCnt;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = CountOdd(iValue);
+    
+    printf("Count of Odd digits in number is : %d",iRet);
+    
+    return 0;
+}
