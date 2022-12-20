20fef7ba9a63675f68ac8ccced03a43ed85ffd9a
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-20 21:22:21 +0530

    Add files via upload

---
+++ Assignment 15/15_4_pattern.c
@@ -0,0 +1,58 @@
+/*
+
+4. Accept number of rows and number of columns from user and display below
+pattern.
+
+Input : iRow = 6 iCol = 6
+Output :
+
+*   *   *   *   *   *
+*   #   #   #   *   *
+*   #   #   *   $   *
+*   #   *   $   $   *
+*   *   $   $   $   *
+*   *   *   *   *   *
+
+*/
+
+#include<stdio.h>
+void Pattern(int iRow, int iCol);
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter Cols : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
+
+void Pattern(int iRow, int iCol)
+{   
+    for(int i = iRow; i >= 1; i--)
+    {
+        for(int j = 1; j <= iCol; j++)
+        {
+            if ((i==j) || (i==iRow) || (i==1) || (j==iCol) || (j==1))
+            {
+                printf("*\t");
+            }
+            else if(i>j)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("$\t");
+            }
+        }
+
+        printf("\n");
+    }
+}
