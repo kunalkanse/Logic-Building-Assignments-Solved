2b84a7bea70198573c436ddc7b5267bb32f4cf2d
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-12-21 23:50:40 +0530

    Create 17_1_Array_FreqEven.c

---
+++ Assignment 17 Arrays/17_1_Array_FreqEven.c
@@ -0,0 +1,61 @@
+/*
+
+1. Accept N numbers from user and return frequency of even numbers.
+Input : N : 6
+
+Elements :85 66 3 80 93 88
+
+Output : 3
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Frequency(int *, int);
+
+int main()
+{
+    int *Arr = NULL;
+    int iLength = 0, iRet = 0;
+
+    printf("Enter the size of Array : ");
+    scanf("%d",&iLength);
+
+    Arr = (int*) malloc (sizeof(int) * iLength);
+
+    if(Arr == NULL)
+    {
+        return -1;
+    }
+
+    printf("Enter Elements : ");
+
+    for(int iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Arr[iCnt]);
+    }
+
+    iRet = Frequency(Arr , iLength);
+
+    printf("Frequency of even elements : %d",iRet);
+
+    free(Arr);
+
+    return 0;
+}
+
+int Frequency(int Brr[] , int iSize)
+{
+    int iEven = 0;
+    
+    for(int iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if((Brr[iCnt] % 2) == 0)
+        {
+            iEven++;
+        }
+    }
+
+    return iEven;
+}
