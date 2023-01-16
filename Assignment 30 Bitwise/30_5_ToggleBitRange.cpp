c5116f261d6a11ad7d649c30fe7889e3d2896947
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2023-01-16 22:34:17 +0530

    Create 30_5_ToggleBitRange.cpp

---
+++ Assignment 30 Bitwise/30_5_ToggleBitRange.cpp
@@ -0,0 +1,11 @@
+
+/*
+
+5. Write a program which accept one number from user and range of
+positions from user. Toggle all bits from that range.
+
+Input : 897 9 13
+Toggle all bits from position 9 to 13 of input number ie 879.
+
+*/
+
