3966cb1ccf6c76ef91ecd12d8383605ce7d0158e
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2023-02-10 23:35:14 +0530

    Add files via upload

---
+++ Assignment 40 pattern/Program40_3_Pattern.java
@@ -0,0 +1,75 @@
+
+import java.util.*;
+
+/*
+
+3. Write a java program which accept number of rows and number of
+columns from user and display below pattern.
+
+Input : iRow = 6 iCol = 6
+
+Output : 
+
+*   *   *   *   *   *
+*               *   *
+*           *       *
+*       *           *
+*   *               *
+*   *   *   *   *   *
+
+*/
+
+class Pattern
+{
+    public int iRows = 0;
+    public int iCols = 0;
+
+    public Pattern(int rows , int cols)
+    {
+        iRows = rows;
+        iCols = cols;
+    }
+
+    public void DisplayPattern()
+    {
+        int iCnt = 0, jCnt = 0;
+
+        for(iCnt = iRows; iCnt >= 1 ; iCnt--)
+        {
+            for(jCnt =  1; jCnt <= iCols; jCnt++)
+            {   
+                if((iCnt==jCnt) || (jCnt==1) || (jCnt==iCols) || (iCnt==1) || (iCnt==iRows))
+                {
+                    System.out.print("*  ");
+                }
+                else
+                {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Program40_3_Pattern 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the Rows : ");
+        int Rows = sobj.nextInt();
+
+        System.out.println("Enter the colomns : ");
+        int Cols = sobj.nextInt();
+
+        Pattern pobj = new Pattern(Rows,Cols);
+
+        pobj.DisplayPattern();
+        
+    }    
+}
+
+
+
